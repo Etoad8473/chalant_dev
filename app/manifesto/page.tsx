ded0ddb266d552c9029bb8466cb8936145452d63
Ezra <62970784+Etoad8473@users.noreply.github.com>
2025-10-10 11:58:32 -0500

    second page created. Next: read blog post markdown files.

---
+++ app/manifesto/page.tsx
@@ -0,0 +1,7 @@
+export default function Manifesto(){
+    return (
+        <div>
+            this is the manifesto page with all my ideas
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
+    <>
+    <head>
+      <link rel="icon" href="/favicon.ico"/>
+    </head>
     <div className="">
       <p>hello, this is ezras website</p>
+      <Link href="/manifesto">manifesto</Link>
     </div>
+    </>
   );
 }
