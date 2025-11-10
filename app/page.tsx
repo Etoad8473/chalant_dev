cc421d77811a85e801c411bfcd041d40d8590674
Ezra <ezra8473@gmail.com>
2025-11-10 17:21:14 -0500

    footer joke positioned correctly

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-    <div className="flex justif-center items-center flex-col">
-      <p>hello, this is ezras website</p>
-      <Link href="/manifesto">manifesto</Link>
-      <Link href="/ideas">ideas</Link>
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <div className="flex-grow flex-col text-center">
+        <p>hello, this is ezras website</p>
+        <Link className="block" href="/manifesto">manifesto</Link>
+        <Link className="block" href="/ideas">ideas</Link>
+      </div>
+      <div className="">
+        <p>no, there are no back buttons</p>
+      </div>
     </div>
+
     </>
   );
 }
